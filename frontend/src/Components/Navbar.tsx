a7437a4114ddd691932dc49ab16f6908500f7be9
Jaseem <muhammadjaseem791@gmail.com>
2024-12-16 23:16:57 +0530

    fixed some micro errors

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className=' max-h-16 mx-auto px-2 flex justify-between'>
-        <div >Blogger</div>
-        <div className='w-36 rounded-full border-slate-500/15 none sm:block' >Sreach bar</div>
-        <div >
-            <div className='block sm:none'>Search Icon</div>
-            <div>Acc</div>
-        </div>
+      <div >Blogger</div>
+      <div className='w-36 rounded-full border-slate-500/15 hidden sm:block' >Sreach bar</div>
+      <div className='flex gap-4  '>
+        <div className='block sm:hidden'>Search Icon</div>
+        <div>Acc</div>
+      </div>
     </div>
   )
 }
