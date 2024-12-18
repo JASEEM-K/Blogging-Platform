24a00450a09c0fe98f9e480c4a95670317e9cc19
Jaseem <muhammadjaseem791@gmail.com>
2024-12-18 13:10:58 +0530

    fixed theme toggle for real

--- frontend/src/Components/mode-togglet.tsx
+++ frontend/src/Components/mode-togglet.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { Moon, Sun } from "lucide-react"
 
 import { Button } from "./ui/button"
@@ -7,11 +6,19 @@ import { useTheme } from "./theme-provider"
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
 
-  return (
-    <Button variant="outline" size="icon">
-      {theme === 'dark' && <Sun onClick={() => setTheme('light')} className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />}
-      {theme === 'light' && <Moon onClick={() => setTheme('dark')} className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />}
-      {theme === 'system' && <Moon onClick={() => setTheme('dark')} className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />}
-    </Button>
-  )
+  if (theme === 'light') {
+    return (
+      <Button variant="outline" size="icon" onClick={() => setTheme('dark')} >
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      </Button>
+    )
+  }
+
+  else if (theme === 'dark') {
+    return (
+      <Button variant="outline" size="icon" onClick={() => setTheme('light')} >
+        <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </Button>
+    )
+  }
 }
