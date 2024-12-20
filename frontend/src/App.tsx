168efb96efa81961dcd0771839522074afa7236b
Jaseem <muhammadjaseem791@gmail.com>
2024-12-20 14:33:37 +0000

    Login Page is getting better

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import Navbar from './components/Navbar'
 import { LoginPage } from './pages/loging-page'
-import { BlogPost } from './components/blog-post'
-import RichTextEditor from './RichTextEditor'
 
 function App() {
 
@@ -9,15 +7,6 @@ function App() {
     <div className=' max-w-screen-lg h-full mx-auto  '>
       <Navbar />
       <LoginPage />
-      <RichTextEditor />
-      <BlogPost
-        title='Frist post'
-        key='hello'
-        author='hello'
-        content='how are you'
-        like={24}
-        comment={3}
-      />
     </div>
   )
 }
--- frontend/src/pages/loging-page.tsx
+++ frontend/src/pages/loging-page.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Eye, EyeClosed, KeyRound, UserRound } from 'lucide-react'
-import { Input } from "../components/ui/input"
 import { Button } from '../components/ui/button'
 
 export const LoginPage = () => {
@@ -12,29 +11,35 @@ export const LoginPage = () => {
 
 
   return (
-    <div className=' max-w-screen-sm mx-auto border border-slate-500/15 rounded-lg flex flex-col justify-center items-center space-y-4 bg-primary/10 '>
-      <h1 className='text-3xl mt-2 font-bold'>Welcome Back</h1>
-      <p className='opacity-85 '> Login to you&apos;re account</p>
+    <div className='font-mono max-w-screen-sm mx-auto p-4 py-8 border border-slate-500/15 rounded-lg flex flex-col h-full justify-center items-center align-middle space-y-4   '>
+      <h1 className='text-4xl mt-2 font-bold'>Welcome Back</h1>
+      <p className='opacity-60'> Login to you&apos;re account</p>
       <form className='space-y-4 flex flex-col max-w-screen-sm items-center'>
-        <div className='flex gap-2 border border-slate-500/10 w-full  rounded-lg px-4 focus:outline-primary  '>
-          <UserRound size='20' />
-          <Input value={formData.username} name='username' placeholder='Username' type='text'
-            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-            className='focus:outline-none bg-transparent '
-          />
-        </div>
-        <div className=' flex gap-2 border border-slate-500/10 w-full rounded-lg justify-between px-4 '>
-          <div className='flex items-center' >
-            <KeyRound className='rotate-12' size='20' />
+        <div className='space-y-1.5 w-full'>
+          <label>Username </label>
+          <div className='flex gap-2 border border-slate-500/10   rounded-lg px-4 focus:outline-primary  '>
+            <UserRound size='20' />
+            <input value={formData.username} name='username' placeholder='Username' type='text'
+              onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+              className='focus:outline-none bg-transparent '
+            />
           </div>
-          <Input value={formData.password} placeholder='Password' type={showPassword}
-            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-            className='focus:outline-none bg-transparent w-full '
-          />
-          <div className='flex items-center cursor-pointer'>
-            {showPassword === 'password' ?
-              <EyeClosed onClick={() => setShowPassword('text')} /> :
-              <Eye onClick={() => setShowPassword('password')} />}
+        </div>
+        <div className='space-y-1.5 w-full'>
+          <label>Password </label>
+          <div className=' flex gap-2 border border-slate-500/10 rounded-lg justify-between px-4 '>
+            <div className='flex items-center' >
+              <KeyRound className='rotate-12' size='20' />
+            </div>
+            <input value={formData.password} placeholder='Password' type={showPassword}
+              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              className='focus:outline-none bg-transparent w-full '
+            />
+            <div className='flex items-center cursor-pointer'>
+              {showPassword === 'password' ?
+                <EyeClosed onClick={() => setShowPassword('text')} /> :
+                <Eye onClick={() => setShowPassword('password')} />}
+            </div>
           </div>
         </div>
         <Button className='w-full'>
