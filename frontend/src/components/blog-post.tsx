b4f4ecfb414046c9b9420a0dc68d8afb47c9978b
Jaseem <muhammadjaseem791@gmail.com>
2024-12-22 10:16:15 +0000

    working with blog-post component

--- frontend/src/components/blog-post.tsx
+++ frontend/src/components/blog-post.tsx
@@ -1,33 +1,29 @@
-import React from "react"
 import { Trash2, Heart, MessageSquareText } from 'lucide-react'
 
 type BlogPostProps = {
-  title: string,
   author: string,
   content: string,
   like: number,
   comment: number,
 }
 
-export const BlogPost = ({ title, author, content, like, comment }: BlogPostProps) => {
+export const BlogPost = ({ author, content, like, comment }: BlogPostProps) => {
+
 
   return (
-    <div>
-      <div className=" flex justify-between">
-        <h1 className="text-2xl">
-          {title}
-        </h1>
+    <div className="font-mono border border-secondary p-4">
+      <div className=" flex justify-end">
         {/* Delete Icon */}
         <Trash2 className="hover:text-red-500" />
       </div>
       <div>{author}</div>
       {/* To show the contennts of the blog Post */}
-      <div>
-        {content}
+      <div className="overflow-hidden max-h-80 w-full p-4 mb-2 rounded-lg  ">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
 
       {/* the controls bar of the blog post */}
-      <div className="flex justify-start gap-4">
+      <div className="flex justify-end gap-4 pt-2 border-t border-secondary ">
         <div className="flex gap-2">
           <Heart />
           {like}
