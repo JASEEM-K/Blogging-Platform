05f1ac18cea95d2d44f06cb6ce9e17d453e5a3fa
Jaseem <muhammadjaseem791@gmail.com>
2024-12-23 08:45:32 +0000

    Text editor is getting closer to ready

--- frontend/src/RichTextEditor.tsx
+++ frontend/src/RichTextEditor.tsx
@@ -3,7 +3,20 @@ import StarterKit from '@tiptap/starter-kit'
 import Typography from '@tiptap/extension-typography'
 import Highlight from '@tiptap/extension-highlight'
 
-import './style.scss'
+import {
+  Undo2,
+  Redo2,
+  Bold,
+  Underline,
+  Italic,
+  Code,
+  Strikethrough,
+  Quote,
+  List,
+  ALargeSmall,
+  ListOrdered,
+
+} from 'lucide-react'
 
 const RichTextEditor = () => {
   const editor = useEditor({
@@ -15,17 +28,85 @@ const RichTextEditor = () => {
     content: '<p>Hello World!</p>',
   })
 
-  const handleLogthis = () => {
-    const html = editor?.getHTML()
-    const htmlString = JSON.stringify(html)
-    console.log(htmlString);
-  }
 
   return (
     <div>
-      <div id='resulting-code-space' />
-      <EditorContent className=' background-color:white ' editor={editor} />
-      <button onClick={handleLogthis} > Print </button>
+
+      <div>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleBold().run()}
+          className={editor?.isActive('bold') ? 'is-active' : ''}
+        >
+          <Bold size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleItalic().run()}
+          className={editor?.isActive('italic') ? 'is-active' : ''}
+        >
+          <Italic size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleStrike().run()}
+          className={editor?.isActive('strike') ? 'is-active' : ''}
+        >
+          <Strikethrough size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleCode().run()}
+          className={editor?.isActive('code') ? 'is-active' : ''}
+        >
+          <Underline size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleCodeBlock().run()}
+          className={editor?.isActive('codeblock') ? 'is-active' : ''}
+        >
+          <Code size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleBulletList().run()}
+          className={editor?.isActive('bulletList') ? 'is-active' : ''}
+        >
+          <List size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleOrderedList().run()}
+          className={editor?.isActive('orderedList') ? 'is-active' : ''}
+        >
+          <ListOrdered size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().toggleBlockquote().run()}
+          className={editor?.isActive('blockquote') ? 'is-active' : ''}
+        >
+          <Quote size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().undo().run()}
+          disabled={!editor?.can().chain().focus().undo().run()} >
+          <Undo2 size={20} />
+        </button>
+
+        <button
+          onClick={() => editor?.chain().focus().redo().run()}
+          disabled={!editor?.can().chain().focus().redo().run()} >
+          <Redo2 size={20} />
+        </button>
+
+      </div>
+
+      <div>
+        <EditorContent className='background-color:white ' editor={editor} />
+      </div>
     </div>
   )
 }
