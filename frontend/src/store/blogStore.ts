023b05ff2afc93d8ef2e57be010142f32f214d83
Jaseem <muhammadjaseem791@gmail.com>
2024-12-25 10:55:21 +0000

    Basic store done now just the testing and such left

--- frontend/src/store/blogStore.ts
+++ frontend/src/store/blogStore.ts
@@ -6,23 +6,24 @@ interface BlogState {
 	blogData: IBlog | null,
 	createBlog: (content: string) => Promise<void>,
 	deleteBlog: (id: string) => Promise<void>,
-	editBlog: () => Promise<void>,
-	likeBlog: () => Promise<void>,
-	commentOnBlog: () => Promise<void>,
-	isCreatingBlog: boolean,
+	editBlog: (content: string, id: string) => Promise<void>,
+	likeBlog: (id: string) => Promise<void>,
+	commentOnBlog: (content: string, id: string) => Promise<void>,
+	isSavingBlog: boolean,
 	isDeletingBlog: boolean
-
+	isCommenting: boolean,
 }
 
 
 export const useBlogStore = create<BlogState>((set) => ({
 
 	blogData: null,
-	isCreatingBlog: false,
+	isSavingBlog: false,
 	isDeletingBlog: false,
+	isCommenting: false,
 
 	createBlog: async (content: string) => {
-		set({ isCreatingBlog: true })
+		set({ isSavingBlog: true })
 		try {
 			const res = await axiosInstance.post<{ data: IBlog }>('/blog/create', content)
 			set({ blogData: res.data as IBlog })
@@ -30,7 +31,7 @@ export const useBlogStore = create<BlogState>((set) => ({
 			console.log("Error when creating Blog Post ", error);
 			set({ blogData: null })
 		} finally {
-			set({ isCreatingBlog: false })
+			set({ isSavingBlog: false })
 		}
 	},
 
@@ -43,15 +44,48 @@ export const useBlogStore = create<BlogState>((set) => ({
 			console.log("Error when Deleting Blog Post ", error);
 			set({ blogData: null })
 		} finally {
-			set({ isDeletingBlog: true })
+			set({ isDeletingBlog: false })
 		}
 	},
 
-	editBlog: async () => { },
+	editBlog: async (content: string, id: string) => {
+		set({ isSavingBlog: true })
+		try {
+			const res = await axiosInstance.post<{ data: IBlog }>(`/blog/edit/${id}`, content)
+			set({ blogData: res.data as IBlog })
+		} catch (error) {
+			console.log("Error when creating Blog Post ", error);
+			set({ blogData: null })
+		} finally {
+			set({ isSavingBlog: false })
+		}
+	},
 
-	likeBlog: async () => { },
+	likeBlog: async (id: string) => {
+		set({ isDeletingBlog: true })
+		try {
+			const res = await axiosInstance.post<{ data: IBlog }>(`/blog/like/${id}`)
+			set({ blogData: res.data as IBlog })
+		} catch (error) {
+			console.log("Error when Deleting Blog Post ", error);
+			set({ blogData: null })
+		} finally {
+			set({ isDeletingBlog: false })
+		}
+	},
 
-	commentOnBlog: async () => { },
+	commentOnBlog: async (content: string, id: string) => {
+		set({ isCommenting: true })
+		try {
+			const res = await axiosInstance.post<{ data: IBlog }>(`/blog/edit/${id}`, content)
+			set({ blogData: res.data as IBlog })
+		} catch (error) {
+			console.log("Error when creating Blog Post ", error);
+			set({ blogData: null })
+		} finally {
+			set({ isCommenting: false })
+		}
+	},
 
 })
 
